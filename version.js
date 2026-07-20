d64615b541136a7bf20f3d292db4939cb2bc8582
mkzns46-tech <mkzns46@gmail.com>
2026-07-20 10:54:33 +0900

    Bump app version to 2.00

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (function(){
-  const version="Ver 1.00";
+  const version="Ver 2.00";
   window.ARICO_APP_VERSION=version;
   function addVersionBadge(){
     if(document.getElementById("aricoAppVersionBadge"))return;
