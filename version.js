eeefc6e0ac7c9b33cfcdc52e64d3119bf0f4c9d3
mkzns46-tech <mkzns46@gmail.com>
2026-08-31 13:30:11 +0900

    lock event report sales quantity to Smaregi

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (function(){
-  const version="Ver 2.93.04";
+  const version="Ver 2.93.05";
   window.ARICO_APP_VERSION=version;
   function addVersionBadge(){
     if(document.getElementById("aricoAppVersionBadge"))return;
