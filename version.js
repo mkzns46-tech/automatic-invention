cd1de589176cff89dfd47b2b1addef60bd8888fb
mkzns46-tech <mkzns46@gmail.com>
2026-09-14 13:10:00 +0900

    Unify return CSV product lookup

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (function(){
-  const version="Ver 2.93.44";
+  const version="Ver 2.93.45";
   window.ARICO_APP_VERSION=version;
   function addVersionBadge(){
     if(document.getElementById("aricoAppVersionBadge"))return;
